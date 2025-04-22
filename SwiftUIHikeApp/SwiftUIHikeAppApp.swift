b6919b80f93d335631dcb04dcfdceff8cb07063f
gouthamdevaraju <gouthamdevaraju@gmail.com>
2025-04-22 13:50:27 +0530

    Added initial files

---
+++ SwiftUIHikeApp/SwiftUIHikeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIHikeAppApp.swift
+//  SwiftUIHikeApp
+//
+//  Created by Goutham Devaraju on 22/04/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIHikeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
