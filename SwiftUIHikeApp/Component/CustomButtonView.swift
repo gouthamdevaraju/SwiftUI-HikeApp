3e97a297c8c1971e3731bda1f5ffc8ca3919e57a
gouthamdevaraju <gouthamdevaraju@gmail.com>
2025-04-22 17:07:03 +0530

    Cusomt buttom

---
+++ SwiftUIHikeApp/Component/CustomButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomButtonView.swift
+//  SwiftUIHikeApp
+//
+//  Created by Goutham Devaraju on 22/04/25.
+//
+
+import SwiftUI
+
+struct CustomButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomButtonView()
+}
